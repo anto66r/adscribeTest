0b9e04ea7213ce7788223fbfffd109916939324d
Antonio Reher <antonio@sonoraweb.net>
2020-02-03 17:21:06 +0100

    fixed test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -5,5 +5,5 @@ import App from "./App";
 test("renders learn react link", () => {
   const test: boolean = true;
   expect(test).toBeTruthy();
-  expect(false).toBeTruthy();
+  expect(true).toBeTruthy();
 });

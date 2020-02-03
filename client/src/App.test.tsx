218c23a1256c2ef50c159e37545b705d7e88a5cd
Antonio Reher <antonio@sonoraweb.net>
2020-02-03 17:19:20 +0100

    test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const test:boolean = true;
+test("renders learn react link", () => {
+  const test: boolean = true;
   expect(test).toBeTruthy();
+  expect(false).toBeTruthy();
 });
-

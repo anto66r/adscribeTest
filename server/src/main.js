1269bf67e2853ae8ebbdbb4b761b83a31b534f7d
Antonio Reher <antonio@sonoraweb.net>
2020-02-03 17:03:49 +0100

    added github workflow for PRs

--- server/src/main.js
+++ server/src/main.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
-const logger = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const createError = require("http-errors");
@@ -10,31 +9,32 @@ const cors = require("cors");
 const CognitoExpress = require("cognito-express");
 
 const app = express();
-
-app.use(logger("dev"));
+if (process.env.ENVIRONMENT !== "production") {
+  const logger = require("morgan");
+  app.use(logger("dev"));
+}
 app.use(cors());
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "jade");
-
+app.disable("etag");
 app.use("/", require("./api")(app));
 
 // Initialize Mongoose
 const uri = process.env.MONGO_URI;
-console.log(uri);
-// mongoose.connect(uri, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true
-// });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  dbName: "platf0rm-2"
+});
 
-// const connection = mongoose.connection;
-// connection.once("open", () => {
-//   console.log("MongoDB database connection established successfully");
-// });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

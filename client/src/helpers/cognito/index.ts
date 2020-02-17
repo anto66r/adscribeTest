289e1779985e153e18d07ede081c391caedd9f73
Javier Olmo <olmogil@gmail.com>
2020-02-17 15:50:33 +0100

    master: Remove unnecesary cognito old authentication

--- client/src/helpers/cognito/index.ts
+++ client/src/helpers/cognito/index.ts
@@ -14,84 +14,9 @@ import { ICognitoAuthentication } from '../../context/UserContext/types';
 import { deleteCookie, getCookie, setCookie } from '../cookies';
 import { ILoginResult, ICognitoSessionModel } from './types';
 
-
 Amplify.configure(awsConfig);
 
 AWSConfig.region = process.env.REACT_APP_COGNITO_REGION;
-
-const tokenScopes = [
-  'openid',
-  'email',
-  'profile',
-  'http://localhost:5000/api',
-  'http://platf0rm-2-lb-1034170497.eu-west-1.elb.amazonaws.com/api',
-  'transactions/api',
-  'transactions/post',
-  'transactions/get',
-  'api',
-  'post',
-  'get',
-];
-
-const {
-  REACT_APP_USER_POOL_BASE_URL: userPoolBaseUrl = '',
-  REACT_APP_COGNITO_USER_POOL: userPool = '',
-  REACT_APP_COGNITO_CLIENT_ID: clientId = '',
-  REACT_APP_COGNITO_SIGNOUT_URL: signoutUri = '',
-} = process.env;
-
-
-// Creates a CognitoAuth instance
-const createCognitoAuth = () => {
-  const appWebDomain = userPoolBaseUrl.replace('https://', '').replace('http://', '');
-  const auth = new CognitoAuth({
-    UserPoolId: userPool,
-    ClientId: clientId,
-    AppWebDomain: appWebDomain,
-    TokenScopesArray: tokenScopes,
-    RedirectUriSignIn: callbackUrl,
-    RedirectUriSignOut: signoutUri,
-  });
-  return auth;
-};
-
-// Creates a CognitoUserPool instance
-const createCognitoUserPool = () => new CognitoUserPool({
-  UserPoolId: userPool,
-  ClientId: clientId,
-});
-
-// Creates a CognitoUser instance
-const createCognitoUser = () => {
-  const pool = createCognitoUserPool();
-  return pool.getCurrentUser();
-};
-
-// Get the URI of the hosted sign in screen
-const getCognitoSignInUri = () => `${userPoolBaseUrl}/login?response_type=code&client_id=${clientId}&redirect_uri=${callbackUrl}`;
-
-// Parse the response from a Cognito callback URI
-// (assumed a token or code is in the supplied href). Returns a promise.
-const parseCognitoWebResponse = (href: string) => new Promise((resolve, reject) => {
-  const auth = createCognitoAuth();
-
-  // userHandler will trigger the promise
-  auth.userhandler = {
-    onSuccess(result: Record<string, any>) {
-      resolve(result);
-    },
-    onFailure(err: string) {
-      reject(new Error(`Failure parsing Cognito web response: ${err}`));
-    },
-  };
-  auth.parseCognitoWebResponse(href);
-});
-
-// Sign out of the current session (will redirect to signout URI)
-const signOutCognitoSession = () => {
-  const auth = createCognitoAuth();
-  auth.signOut();
-};
 
 const setAuthSession = ({
   cognitoUsername,
@@ -210,12 +135,6 @@ const getLoggedUser = (): AuthType => ({
 });
 
 export {
-  createCognitoAuth,
-  createCognitoUser,
-  createCognitoUserPool,
-  getCognitoSignInUri,
-  parseCognitoWebResponse,
-  signOutCognitoSession,
   login,
   getLoggedUser,
   logout,

43552d9400da2f00d9b30f3420468875e0368292
Javier López de Ancos <javier@zartis.com>
2020-02-28 11:17:55 +0100

    P2-72: configure storybook to get tsx stories

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*stories.[tj]sx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',

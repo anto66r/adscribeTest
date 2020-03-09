e020c24da0414daa892e1f13d5651d7e04c6e16f
Javier López de Ancos <javier@zartis.com>
2020-03-09 15:58:27 +0100

    P2-119 fix lint errors

--- client/src/components/Button/stories.tsx
+++ client/src/components/Button/stories.tsx
@@ -1,18 +1,18 @@
 import { action } from '@storybook/addon-actions';
 import React, { ReactElement } from 'react';
-import Button, {Kind} from './index';
-import Flex, { Direction, Horizontal } from '../Flex'
+import Button, { Kind } from './index';
+import Flex, { Direction, Horizontal } from '../Flex';
 
 export default { title: 'Button' };
 
 export const Primary = (): ReactElement => (
   <Flex direction={Direction.column} horizontal={Horizontal.left}>
     <Button kind={Kind.primary} onClick={action('button-click')}>
-        Primary
+      Primary
     </Button>
 
-    <Button kind={Kind.primary} disabled={true} onClick={action('button-click')}>
-        Primary
+    <Button kind={Kind.primary} disabled onClick={action('button-click')}>
+      Primary
     </Button>
   </Flex>
 );

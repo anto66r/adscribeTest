cf87ed49cc3e100b30502de1887c83f35011e2b2
Javier López de Ancos <javier@zartis.com>
2020-03-12 08:35:39 +0100

    P2-73 fix typo

--- client/.storybook/manager.js
+++ client/.storybook/manager.js
@@ -1,13 +1,13 @@
 import { addons } from '@storybook/addons';
 import adscribe from './themes/adscribe';
-import sixZeroOne from './themes/605';
+import sixZeroFive from './themes/605';
 
 let theme;
 console.warn(process.env.STORYBOOK_THEME)
 if (process.env.STORYBOOK_THEME === 'adscribe') {
   theme = adscribe;
 } else if (process.env.STORYBOOK_THEME === '605') {
-  theme = sixZeroOne;
+  theme = sixZeroFive;
 }
 
 addons.setConfig({
